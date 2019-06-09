73ea6712bf68491f05fd5cb55f141ce10aef639c
João Paulo Nascimento Gardiano <46694429+diczr@users.noreply.github.com>
2019-06-09 00:43:03 -0300

    Add: Film List and @Media
    Javascript puro/vanilla

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,6 @@
 
-// add item ao botão de pesquisa;
+
+            // id's html;
         let button = document.querySelector('#botao');
         let name = document.querySelector('#Name');
         let population = document.querySelector('#Population');
@@ -8,26 +9,36 @@
         let films = document.querySelector('#filmes');
 
 
-        // aleatoriedade e getApi Func
-function getPlanet() {
-        let randomNumber = Math.floor(( Math.random() * 10 ) + 1 )
-        let apiUrl       = 'https://swapi.co/api/planets/' + randomNumber
+        // aleatoriedade e getApi Func;
+    async function getPlanet() {
+        let Random = Math.floor(( Math.random() * 10 ) + 1 )
+        let api = 'https://swapi.co/api/planets/' + Random
 
-            // integrando API para adicionar a função getPlanet;
-        axios.get (apiUrl).then(( function ( response ) {        
-        updateInfo (response.data)
-      
-    } ))      
+        // integrando API para retornar planetas e filmes;
+        let planet = await axios.get(api)
+        let films = await Promise.all (
+            planet.data.films.map(axios.get)
+        )
+            // arrays de planetas e filmes;
+        let planetData = planet.data
+        let filmsData = films.map(film => film.data)
+
+        updateInfo(planetData, filmsData)     
 }
+        // Adicionando retorno da api ao html;
+    function updateInfo(planetData, filmsData) {   
+    
+        let titles = filmsData.map(filmData => filmData.title)
 
-function updateInfo(data) {   
-
-        name.innerText = 'Planeta: ' + data.name;
-        population.innerText = 'População total: ' + data.population;
-        climate.innerText = 'Clima: ' + data.climate;
-        terrain.innerText = 'Terreno: ' + data.terrain;    
-        films.innerText = 'Aparição em filmes: ' + data.films;
+        name.innerText = 'Planeta: ' + planetData.name;
+        population.innerText = 'População total: ' + planetData.population;
+        climate.innerText = 'Clima: ' + planetData.climate;
+        terrain.innerText = 'Terreno: ' + planetData.terrain;    
+        films.innerText = 'Aparição em filmes: ' + titles.join(' , ');
     }
 
-        // Evento Click;
-button.addEventListener('click', getPlanet);
+        // Adicionando função getPlanet ao click do botão;
+        button.addEventListener('click', getPlanet);
+
+
+////////////////////////////////////////////////
